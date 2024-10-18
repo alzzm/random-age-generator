8c78212a09c5077b3163e7fa884199445e910649
alzzm <kosalaprasetya@gmail.com>
2024-10-18 07:41:29 +0700

    add base structure and styling, modify some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
-import './App.css'
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Style from './App.module.css'
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className={Style.main}>
+      <div className={Style.app}>
+        <input
+          placeholder='enter your name'
+          className={Style.input}
+        />
+
+        <h3 className={Style.outputTxt}>Your average age: </h3>
+      </div></div>
+  );
 }
 
 export default App
