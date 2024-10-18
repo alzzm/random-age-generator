1285c7a06b1a88236cbd50a4bde7bce1786a8ade
alzzm <kosalaprasetya@gmail.com>
2024-10-18 09:17:54 +0700

    add fetch functionality with axios, functional input name

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,35 @@ import Axios from 'axios';
 import Style from './App.module.css'
 
 function App() {
+
+  const endpoint = 'https://api.agify.io/?name='
+
+  const [name, setName] = useState('')
+  const [data, setData] = useState(null)
+
+  const getData = () => {
+    Axios.get(`${endpoint}${name}`)
+      .then((res) => setData(res.data))
+
+  }
+
   return (
     <div className={Style.main}>
       <div className={Style.app}>
         <input
           placeholder='enter your name'
           className={Style.input}
+          onChange={(event) => setName(event.target.value)}
         />
 
-        <h3 className={Style.outputTxt}>Your average age: </h3>
+        <button
+          onClick={getData}
+          className={Style.button}
+        >
+          Proceed</button>
+
+        <h2 className={Style.outputTxt}>Hey! {data?.name}</h2>
+        <h3 className={Style.outputTxt}>Your average age: {data?.age}</h3>
       </div></div>
   );
 }
